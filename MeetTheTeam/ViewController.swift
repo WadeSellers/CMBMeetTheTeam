57aa59ec1556cd180743e42a53e5e01d85fe9a0b
WadeSellers <Waderoni@gmail.com>
2017-05-21 22:58:38 -0400

    updated JSON to include me

--- MeetTheTeam/ViewController.swift
+++ MeetTheTeam/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testImageView: UIImageView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Begin of code")
+        if let checkedUrl = URL(string: "https://pbs.twimg.com/profile_images/690230312706519041/abGQPwuR.jpg") {
+            testImageView.contentMode = .scaleAspectFit
+            downloadImage(url: checkedUrl)
+        }
+        print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                self.testImageView.image = UIImage(data: data)
+            }
+        }
     }
 
 
