bd22a461102a6bec840c0a821fbcea5c3b9b0fdc
WadeSellers <Waderoni@gmail.com>
2017-05-21 23:22:34 -0400

    setup readme within app

---
+++ MeetTheTeam/ForTheEvaluator.swift
@@ -0,0 +1,30 @@
+//
+//  ForTheEvaluator.swift
+//  MeetTheTeam
+//
+//  Created by Wade Sellers on 5/21/17.
+//  Copyright © 2017 WadeSellers. All rights reserved.
+//
+
+/*
+ 
+ Hello CMB iOS Developer...
+ 
+ I wanted to write a file to answer some questions and clarify things I've done in the code for this submission.
+ 
+ First off... Why the heck did I use SwiftJSON for the smallest of JSON files?
+    Good Question: I did this intentionally to show that I understood how to utilize cocoapods, and work with a 3rd party framework. This is just 1 I've used before and it could be used for relevance in this project. However with a JSON file structure this simplistic, a few Guard statements and I would've been parsed out just fine and moving on but I wanted to show competency in 3rd party frameworks.
+ 
+ 2ndly... Why did you add to the JSON file?
+    Well, it just seemed like a neat way to say hi to you in case I wasn't given an opportunity to after you see this code... SO... HELLO!
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+*/
